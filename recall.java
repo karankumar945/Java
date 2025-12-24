82c7f7c1ed17cf6a18a6fc0e8c91ea2c86cf1fb9
karankumar945 <innocentboy12021@gmail.com>
2025-12-24 16:28:54 +0530

    half code

--- recall.java
+++ recall.java
@@ -254,3 +254,27 @@
 //     }
 // }
 
+
+public class recall{
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length-1;
+        while(high>=low){
+            int mid = high-low/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target < nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid +1;
+            }
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        
+    }
+}
