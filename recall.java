e461f5bf29feb2e57f3e2e42f8ee5b671f091657
karankumar945 <innocentboy12021@gmail.com>
2025-12-30 11:40:44 +0530

    backtrack

--- recall.java
+++ recall.java
@@ -431,3 +431,29 @@
 //         System.out.println(res);
 //     }
 // }
+
+public class recall{
+    public static int countNegatives(int[][] grid) {
+        int count = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j] < 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int grid[][] = {
+    {4, 3, 2, -1},
+    {3, 2, 1, -1},
+    {1, 1, -1, -2},
+    {-1, -1, -2, -3}
+};
+        int res = countNegatives(grid);
+        System.out.println(res);
+    }
+}
